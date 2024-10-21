f9df5f0414817dced3c257839b7e65d18d400830
Moataz Tabl <moataztabl.m2@gmail.com>
2024-10-21 12:22:44 +0300

    created base file for user roles

--- lib/core/app_router.dart
+++ lib/core/app_router.dart
@@ -1,12 +1,17 @@
 import 'package:dahab_delivery/core/app_strings.dart';
+import 'package:dahab_delivery/features/admin/presentation/admin_home_page_screen.dart';
 import 'package:dahab_delivery/features/auth/signin/presentation/sign_in_screen.dart';
 import 'package:dahab_delivery/features/auth/signup/presentation/sign_up_screen.dart';
+import 'package:dahab_delivery/features/driver/presentation/driver_home_page_screen.dart';
+import 'package:dahab_delivery/features/partner/presentation/partner_home_page_screen.dart';
+import 'package:dahab_delivery/features/user/presentation/user_home_page_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouter {
   static final GoRouter router = GoRouter(
     routes: <RouteBase>[
+      // Auth
       GoRoute(
         path: AppStrings.loginScreenRoute,
         builder: (BuildContext context, GoRouterState state) {
@@ -17,6 +22,34 @@ class AppRouter {
         path: AppStrings.signupScreenRoute,
         builder: (BuildContext context, GoRouterState state) {
           return const SignUpScreen();
+        },
+      ),
+    // Admin
+    GoRoute(
+        path: AppStrings.adminHomeScreenRoute,
+        builder: (BuildContext context, GoRouterState state) {
+          return const AdminHomePageScreen();
+        },
+      ),
+    // Driver
+    GoRoute(
+        path: AppStrings.driverHomeScreenRoute,
+        builder: (BuildContext context, GoRouterState state) {
+          return const DriverHomePageScreen();
+        },
+      ),
+    // Partner
+    GoRoute(
+        path: AppStrings.partnerHomeScreenRoute,
+        builder: (BuildContext context, GoRouterState state) {
+          return const PartnerHomePageScreen();
+        },
+      ),
+    // User
+    GoRoute(
+        path: AppStrings.userHomeScreenRoute,
+        builder: (BuildContext context, GoRouterState state) {
+          return const UserHomePageScreen();
         },
       ),
     ],
---
+++ lib/features/admin/presentation/admin_home_page_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AdminHomePageScreen extends StatelessWidget {
+  const AdminHomePageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Center(child: Text("Admin"))
+        ],
+      ),
+    );
+  }
+}
